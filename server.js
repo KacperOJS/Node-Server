8b256e8b6ed28579e56b2b019dccb969343f9e15
Kacper <kacperodziemczyk@gmail.com>
2023-06-28 18:31:03 +0200

    Connection to React by fetch doing same stuff as django

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const app = express();
+const PORT = 3001;
+app.use(cors()); // Add this line to enable CORS
+app.use(bodyParser.json());
+
+let dataObjects = [
+	{
+		name:'Rycho',
+		age:40,
+		info:'bije misie',
+	}
+]; // Array to store the data objects received
+
+app.post('/api/data', (req, res) => {
+  const newData = req.body;
+  dataObjects.push(newData);
+  console.log(JSON.stringify(dataObjects,null,2));
+  console.log('Data received:', newData);
+  res.sendStatus(200); // Send a success status code
+});
+app.delete('/api/data/:index', (req, res) => {
+	const { index } = req.params;
+  
+	// Check if the index is valid
+	if (index >= 0 && index < activeFetch.length) {
+	  // Remove the object at the specified index
+	  activeFetch.splice(index, 1);
+  
+	  // Send a response indicating success
+	  res.json({ success: true });
+	} else {
+	  // Send a response indicating failure
+	  res.status(404).json({ success: false, message: 'Object not found' });
+	}
+  });
+  
+
+
+app.get('/api/data', (req, res) => {
+  res.json(dataObjects); // Send the stored data objects as a response
+});
+
+app.get('/', (req, res) => {
+  res.send('<p>Welcome to the home page!</p>' ); // Send a simple HTML response
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
